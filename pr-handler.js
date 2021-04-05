5b04d9a2bcf6515b63fefefe32e472cf19d2506b
Evyatar Daud <evyatard@elementor.com>
2021-04-05 16:49:52 +0300

    Fixes.

--- pr-handler.js
+++ pr-handler.js
@@ -1,6 +1,7 @@
 const shell = require( 'shelljs' );
 const util = require( 'util' );
 const fs = require( 'fs' );
+const fetch = require( 'node-fetch' );
 const { execSync, exec } = require( 'child_process' );
 const execAsync = util.promisify( exec );
 
@@ -10,23 +11,25 @@ const DB_NAME           = process.env.DB_NAME           || 'tests-db';
 const DB_USER           = process.env.DB_USER           || 'root';
 const DB_PASSWORD       = process.env.DB_PASSWORD       || 'root';
 const DB_HOST           = process.env.DB_HOST           || 'localhost';
+const SLACK_TOKEN 		= process.env.SLACK_TOKEN 		|| '';
+const SLACK_CHANNEL 	= process.env.SLACK_CHANNEL 	|| '';
 
 class PR_Handler
 {
-    /**
-     * Initialize the PR handler.
-     * 
-     * @param {Object} params.head      The PR HEAD commit.
-     * @param {Object} params.app       A Probot app instance.
-     * @param {Object} params.context   A webhook context.
-     */
-    constructor( { head, app, context } )
-    {
-        this.head = head;
-        this.app = app;
-        this.context = context;
+	/**
+	 * Initialize the PR handler.
+	 * 
+	 * @param {Object} params.head      The PR HEAD commit.
+	 * @param {Object} params.app       A Probot app instance.
+	 * @param {Object} params.context   A webhook context.
+	 */
+	constructor( { head, app, context } )
+	{
+		this.head = head;
+		this.app = app;
+		this.context = context;
 
-        // Extract props from HEAD.
+		// Extract props from HEAD.
 		this.commitSHA  = head.sha;
 		this.owner      = head.repo.owner.login;
 		this.repo       = head.repo.name;
@@ -36,16 +39,16 @@ class PR_Handler
 		this.WORKING_DIR    = `/tmp/elementor-tests/${ new Date().getTime() }-${ this.commitSHA }`;
 		this.ELEMENTOR_PATH = `${ this.WORKING_DIR }/elementor/elementor.php`;
 
-        this.handle();
-    }
+		this.handle();
+	}
 
-    /**
-     * Handle an incoming PR.
-     * 
-     * @returns {void}
-     */
-    async handle()
-    {
+	/**
+	 * Handle an incoming PR.
+	 * 
+	 * @returns {void}
+	 */
+	async handle()
+	{
 		this.log( `New PR. Processing...` );
 		
 		// Create a check run.
@@ -59,164 +62,282 @@ class PR_Handler
 		// Clone.
 		try
 		{
-            this.log( 'Cloning repos...' );
+			this.log( 'Cloning repos...' );
 			await this.cloneRepos();
 		}
 
 		catch
 		{
-			this.log( 'ERROR: Cannot clone repos.' );
+			this.error( 'Cannot clone repos.' );
 			return;
 		}
 
 		// Build.
-        try
+		try
 		{
-            this.log( 'Building the projects files...' );
+			this.log( 'Building the projects files...' );
 			await this.runBuild();
 		}
 
-		catch
+		catch( e )
 		{
-			this.log( 'ERROR: Cannot run build commands.' );
+			this.error( 'Cannot run build commands.' );
 			return;
 		}
 		
 		// Setup Pro tests.
-        this.log( 'Setting up tests...' );
-        const { phpunit, qunit } = this.executeTests();
-        
+		let phpunit, jest;
+
+		try
+		{
+			this.log( 'Setting up tests...' );
+			( { phpunit, jest } = this.executeTests() );
+		}
+		
+		catch( e )
+		{
+			this.error( 'Failed executing tests.' );
+		}
 
 		// Determine if the tests were successful.
 		const phpunitSuccess = ( phpunit.code === 0 );
-		const qunitSuccess = ( qunit.code === 0 );
-		const success = ( phpunitSuccess && qunitSuccess );
-		
-		// Set response data.
-		const conclusion = success ? 'success' : 'failure';
-		const title = success ? 'Successful' : 'Failed';
+		const jestSuccess = ( jest.code === 0 );
+		const success = ( phpunitSuccess && jestSuccess );
+
+		const phpunitSuccessMessage = phpunitSuccess ? 'Success' : 'Failed';
+		const jestSuccessMessage = jestSuccess ? 'Success' : 'Failed';
 
 		// Build check summary.
-		let summary = [];
-		summary.push( `### PHPUnit: ${ phpunitSuccess ? 'Success' : 'Failed' }` );
-		summary.push( '```' + phpunit.stdout + '```' );
-		summary.push( `### QUnit: ${ qunitSuccess ? 'Success' : 'Failed' }` );
-		summary.push( '```' + qunit.stdout + '```' );
+		let phpunitSummary = [], jestSummary = [];
 
-		// Send the check run result.
+		phpunitSummary.push( `*PR: ${ this.head.label }*` );
+		phpunitSummary.push( `*PHPUnit: ${ phpunitSuccessMessage }*` );
+		phpunitSummary.push( '```' + phpunit.stdout + phpunit.stderr + '```' );
+
+		jestSummary.push( `*PR: ${ this.head.label }*` );
+		jestSummary.push( `*Jest: ${ jestSuccessMessage }*` );
+		jestSummary.push( '```' + jest.stdout + jest.stderr + '```' );
+
+		// Send the messages to Slack.
+		try
+		{
+			const summary = [];
+
+			if ( ! phpunitSuccess )
+			{
+				const message = await this.slackMessage( phpunitSummary.join( '\n' ) );
+				summary.push( `PHPUnit ( ${ phpunitSuccessMessage } ): ${ message }` );
+			}
+
+			if ( ! jestSuccess )
+			{
+				const message = await this.slackMessage( jestSummary.join( '\n' ) );
+				summary.push( `Jest ( ${ jestSuccessMessage } ): ${ message }` );
+			}
+	
+			// Set response data.
+			const conclusion = success ? 'success' : 'failure';
+			const title = success ? 'Successful' : 'Failed';
+
+			// Send the check run result.
+			await this.githubCheckRun( {
+				conclusion,
+				output: {
+					title,
+					summary: summary.join( '\n' ),
+				},
+			} );
+		}
+
+		catch( e )
+		{
+			this.error( 'Cannot send message to Slack.' );
+		}
+
+		this.cleanup();
+
+		this.log( `Finished processing PR.` );
+	}
+
+	/**
+	 * Output a log message to the console.
+	 * 
+	 * @param {String} message 
+	 * 
+	 * @returns {void}
+	 */
+	log( message )
+	{
+		this.app.log( `[ ${ this.head.label } ] :: ${ message }` );
+	}
+
+	/**
+	 * Log an error to the console & cleanup.
+	 * 
+	 * @param {String} message 
+	 */
+	async error( message )
+	{
 		await this.githubCheckRun( {
-			conclusion,
+			conclusion: 'failure',
 			output: {
-				title,
-				summary: summary.join( '\n' ),
+				title: 'CI Server Error',
+				summary: message,
 			},
 		} );
 
-		shell.exec( `rm -rf ${ WORKING_DIR }` );
+		this.log( `ERROR: ${ message }` );
+		this.cleanup();
+	}
 
-		this.log( `Finished processing PR.` );
-    }
+	/**
+	 * Clean the current working directory.
+	 * 
+	 * @returns {void}
+	 */
+	cleanup()
+	{
+		shell.exec( `rm -rf ${ this.WORKING_DIR }` );
+	}
 
-    /**
-     * Output a log message to the console.
-     * 
-     * @param {string} message 
-     */
-    log( message )
-    {
-        this.app.log( `[ ${ this.head.label } ] :: ${ message }` );
-    }
+	/**
+	 * Create / Update a Github check run.
+	 * 
+	 * @param {Object} data The check run data to send.
+	 * 
+	 * @returns {Promise}
+	 */
+	async githubCheckRun( data )
+	{
+		try
+		{
+			// Extract the base repo owner.
+			const owner = this.context.payload.repository.owner.login;
 
-    /**
-     * Create / Update a Github check run.
-     * 
-     * @param {Object} data The check run data to send.
-     * 
-     * @returns {Promise}
-     */
-    async githubCheckRun( data )
-    {
+			return this.context.octokit.request( 'POST /repos/{owner}/{repo}/check-runs', {
+				owner,
+				repo: this.repo,
+				name: CHECK_RUN_NAME,
+				head_sha: this.commitSHA,
+				...data
+			} );
+		}
 
-        return this.context.octokit.request( 'POST /repos/{owner}/{repo}/check-runs', {
-			owner: this.owner,
-			repo: this.repo,
-			name: CHECK_RUN_NAME,
-			head_sha: this.commitSHA,
-            ...data
-		} );
-    }
+		catch( e )
+		{
+			this.error( 'Cannot send Github check run data.' );
+		}
+	}
 
-    /**
-     * Clone the Core & Pro repos asynchronously.
-     * 
-     * @returns {Promise}
-     */
-    async cloneRepos()
-    {
-        shell.cd( this.WORKING_DIR );
-        shell.exec( 'rm -rf elementor' );
-        shell.exec( 'rm -rf elementor-pro' );
+	/**
+	 * Clone the Core & Pro repos asynchronously.
+	 * 
+	 * @returns {Promise}
+	 */
+	async cloneRepos()
+	{
+		shell.cd( this.WORKING_DIR );
+		shell.exec( 'rm -rf elementor' );
+		shell.exec( 'rm -rf elementor-pro' );
 
-        const token = process.env.ACCESS_TOKEN;
-        const core = execAsync( `git clone --single-branch --branch ${ this.ref } ${ this.repoURL } elementor` );
-        const pro = execAsync( `git clone --single-branch --branch master https://${ token }:x-oauth-basic@github.com/elementor/elementor-pro.git` );
+		const token = process.env.ACCESS_TOKEN;
+		const core = execAsync( `git clone --single-branch --branch ${ this.ref } ${ this.repoURL } elementor` );
+		const pro = execAsync( `git clone --single-branch --branch develop https://${ token }:x-oauth-basic@github.com/elementor/elementor-pro.git` );
 
-        return Promise.all( [ core, pro ] );
-    }
+		return Promise.all( [ core, pro ] );
+	}
 
-    /**
-     * Install & build the JS source.
-     * 
-     * @returns {Promise}
-     */
-    async runBuild()
-    {
-        // Install Core.
-        shell.cd( `${ this.WORKING_DIR }/elementor` );
-        shell.exec( `git checkout ${ this.commitSHA }`, { silent: true } );
-        execSync( 'npm i' );
-        const core = execAsync( `grunt` );
-        
-        // Install Pro.
-        shell.cd( `${ this.WORKING_DIR }/elementor-pro` );
-        execSync( `npm i` );
-        const pro = execAsync( `grunt` );
+	/**
+	 * Install & build the JS source.
+	 * 
+	 * @returns {Promise}
+	 */
+	async runBuild()
+	{
+		// Install Core.
+		shell.cd( `${ this.WORKING_DIR }/elementor` );
+		shell.exec( `git checkout ${ this.commitSHA }`, { silent: true } );
+		const core = execAsync( 'npm i && npx grunt scripts' );
+		
+		// Install Pro.
+		shell.cd( `${ this.WORKING_DIR }/elementor-pro` );
+		const pro = execAsync( 'npm i && npx grunt scripts' );
 
-        return Promise.all( [ core, pro ] );
-    }
+		return Promise.all( [ core, pro ] );
+	}
 
-    /**
-     * Execute the Pro tests.
-     * 
-     * @returns {Object}
-     */
-    executeTests()
-    {
-        shell.cd( `${ this.WORKING_DIR }/elementor-pro` );
-        shell.exec( `bash ./bin/install-wp-tests.sh ${ DB_NAME } ${ DB_USER } ${ DB_PASSWORD } ${ DB_HOST } > /dev/null 2>&1` );
-        execSync( `rm -rf /tmp/wordpress/wp-content/plugins/elementor/` );
+	/**
+	 * Execute the Pro tests.
+	 * 
+	 * @returns {Object}
+	 */
+	executeTests()
+	{
+		shell.cd( `${ this.WORKING_DIR }/elementor-pro` );
+		shell.exec( `bash ./bin/install-wp-tests.sh ${ DB_NAME } ${ DB_USER } ${ DB_PASSWORD } ${ DB_HOST } > /dev/null 2>&1` );
+		execSync( `rm -rf /tmp/wordpress/wp-content/plugins/elementor/` );
 
-        // Install PHPUnit if not exists.
-        // TODO: Remove?
-        if ( ! fs.existsSync( './vendor/bin/phpunit' ) )
-        {
-            execSync( `composer require "phpunit/phpunit:7.5.9" > /dev/null 2>&1` );
-        }
+		// Install PHPUnit if not exists.
+		// TODO: Remove?
+		if ( ! fs.existsSync( './vendor/bin/phpunit' ) )
+		{
+			shell.exec( `composer require "phpunit/phpunit:7.5.9" > /dev/null 2>&1` );
+		}
 
-        // Run tests.
-        this.log( 'Executing PHPUnit...' );
-        const phpunit = shell.exec( `export WP_TESTS_ELEMENTOR_DIR=${ this.ELEMENTOR_PATH } && ./vendor/bin/phpunit -v` );
+		// Run tests.
+		this.log( 'Executing PHPUnit...' );
+		const phpunit = shell.exec( `export WP_TESTS_ELEMENTOR_DIR="${ this.ELEMENTOR_PATH }" && ./vendor/bin/phpunit -v` );
 
-        this.log( 'Executing Qunit...' );
-        const qunit = shell.exec( `grunt karma:unit` );
+		this.log( 'Executing Jest...' );
+		const jest = shell.exec( `npm run test:jest` );
 
-        return {
-            phpunit,
-            qunit,
-        };
-    }
+		return {
+			phpunit,
+			jest,
+		};
+	}
+
+
+	/**
+	 * Send a message to Slack using the Slack API.
+	 * 
+	 * @param {String} text 		The text to send as a message.
+	 * @param {Boolean} isMakrdown 	Determine if the message should be parsed as Markdown.
+	 * 
+	 * @returns {String} 			URL to the created message.
+	 */
+	async slackMessage( text, isMakrdown = true )
+	{
+		const endpoint = 'https://slack.com/api/chat.postMessage';
+
+		const headers = {
+			'Content-type': 'application/json',
+			'Authorization': `Bearer ${ SLACK_TOKEN }`,
+		};
+
+		const data = {
+			text,
+			channel: SLACK_CHANNEL,
+			mrkdwn: isMakrdown,
+		};
+
+		const res = await fetch( endpoint, {
+			method: 'POST',
+			headers,
+			body: JSON.stringify( data ),
+		} ).then( res => res.json() );
+
+		if ( ! res.ok )
+		{
+			return null;
+		}
+
+		const { channel, ts } = res;
+
+		return `https://elementor.slack.com/archives/${ channel }/p${ ts }`;
+	}
 }
 
 module.exports = {
-    PR_Handler,
+	PR_Handler,
 };
